51a280a9671d29b200abf8fa14fc0183cca9c7e7
Your Name <your-email@example.com>
2024-12-30 10:47:43 +0900

    Update: 2024-12-30 10:47:43

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ git commit -m "Update: $(date +"%Y-%m-%d %H:%M:%S")"
 git push origin main
 
 # SSH to server and deploy
-sshpass -p "Sy(3iP8iFBCM=rhm" ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o PubkeyAuthentication=no root@45.32.39.38 "cd /var/www/ais.bit-co.jp && git pull origin main && systemctl restart nginx"
+sshpass -p "Sy(3iP8iFBCM=rhm" ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o PubkeyAuthentication=no root@45.32.39.38 "cd /var/www/ais.bit-co.jp && git fetch origin && git reset --hard origin/main && systemctl restart nginx"
